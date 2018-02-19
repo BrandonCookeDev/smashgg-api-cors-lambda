224b986cec408d5cee70e939109499321b0aa363
EC2 Default User <ec2-user@ip-172-31-27-128.us-west-2.compute.internal>
2018-02-19 01:57:35 +0000

    changed port for server to 8081 for elastic beanstalk

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(routes);
 
-app.listen('8080', () => console.log('Listening on port 8080...')); 
+app.listen('8081', () => console.log('Listening on port 8080...')); 
