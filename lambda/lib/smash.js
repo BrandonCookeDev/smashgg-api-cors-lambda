8bb56b2553c83c3cecaa9080181437e6535532f0
Brandon Cooke <brandoncookedev@gmail.com>
2018-08-14 00:25:22 -0400

    Updated smashgg library

--- lambda/lib/smash.js
+++ lambda/lib/smash.js
@@ -6,79 +6,67 @@ const smashgg = require('smashgg.js');
 class Smash{
 
     static tournament(tournamentName, expands){
-        return new Promise(function(resolve, reject){
-            // /name, expands
-            try{
-                let Tournament = new smashgg.Tournament(tournamentName, expands);
-                Tournament.on('ready', function(){
-                    return resolve(Tournament.data);
-                })
-            } catch(err){
+        return smashgg.Tournament.getTournament(tournamentName, {expands: expands})
+            .catch((e) => { 
                 console.error(err);
-                return reject(err);
-            }
-        })
+                throw (err);
+            })
     }
     
     static event(tournamentName, eventName, expands){
-        return new Promise(function(resolve, reject){
-            //tournamentName, eventName, expands
-            try{
-                let Event = new smashgg.Event(tournamentName, eventName, expands);
-                Event.on('ready', () => {
-                    return resolve(Event.data);
-                })
-            } catch(err){
+        return smashgg.Event.getEvent(eventName, tournamentName, {expands: expands})
+            .catch((e) => { 
                 console.error(err);
-                returnreject(err);
-            }
-        })
+                throw (err);
+            })
     }
     
     static phase(id, expands){
-        return new Promise(function(resolve, reject){
-            //id, expands
-            try {
-                let Phase = new smashgg.Phase(id, expands);
-                Phase.on('ready', function(){
-                    return resolve(Phase.data);
-                })
-            } catch(err) {
-                console.log(err);
-                return reject(err);
-            }
-        });
+        return smashgg.Phase.getPhase(id, {expands: expands})
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
     
     static phasegroup(id, expands){
-        return new Promise(function(resolve, reject){
-            //id, expands
-            try {
-                let PhaseGroup = new smashgg.PhaseGroup(id, expands);
-                PhaseGroup.on('ready', () => {
-                    return resolve(PhaseGroup.data);
-                })
-            } catch(e) {
-                console.log(e);
-                return reject(e);
-            }
-        })
+        return smashgg.PhaseGroup.getPhaseGroup(id, {expands: expands})
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
 
     static set(id){
-        return smashgg.Set.getSet(id);
+        return smashgg.Set.getSet(id)
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
 
     static sets(idArray){
-        return Promise.all(idArray.map(id => { return smashgg.Set.getSet(id); }));
+        return Promise.all(idArray.map(id => { return smashgg.Set.getSet(id); }))
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
 
     static player(id){
-        return smashgg.Player.getPlayer(id);
+        return smashgg.Player.getPlayer(id)
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
     
     static players(idArray){
-        return Promise.all(idArray.map(id => { return smashgg.Player.getPlayer(id); }));
+        return Promise.all(idArray.map(id => { return smashgg.Player.getPlayer(id); }))
+            .catch((e) => { 
+                console.error(err);
+                throw (err);
+            })
     }
 
     static tournamentPhaseGroups(tournamentName, expands){
